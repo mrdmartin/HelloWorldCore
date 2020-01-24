1e399b0395917c701131b2a5589150dc34270cde
David Martín <david.martin.romero@outlook.com>
2020-01-24 17:14:31 +0100

    Agregar archivos de proyecto.

---
+++ HelloWorldTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWorldTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelloWorldTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c90c2f15-dac1-4759-8e55-6a556f11fcaa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HelloWorldTests/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace HelloWorldTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string Expected = "Hello World!";
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                HelloWorldFrameWork.Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected, result);
+
+
+                //Console.WriteLine("Ingrese un numero a sumar:");
+                //int numero1 = Console.Read();
+                // Console.WriteLine("Ingrese otro numero a sumar:");
+                //int numero2 = Console.Read();
+
+                //int resultado = numero1 + numero2;
+
+                //Console.WriteLine("El resultado es", resultado);
+            }
+        }
+    }
+}
